33defbfbc4bbd1ea35526b264091579373049feb
EC2 Default User <ec2-user@ip-172-31-19-208.ap-northeast-1.compute.internal>
2023-09-09 10:44:30 +0000

    [add]model

---
+++ db/migrate/20230909103641_create_facility_categories.rb
@@ -0,0 +1,8 @@
+class CreateFacilityCategories < ActiveRecord::Migration[6.1]
+  def change
+    create_table :facility_categories do |t|
+      t.string :category_name, null: false
+      t.timestamps
+    end
+  end
+end
