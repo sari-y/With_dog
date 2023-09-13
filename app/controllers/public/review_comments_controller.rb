e4ec0b69c5a3bd76c672913e5c246a81d1fe2bbd
EC2 Default User <ec2-user@ip-172-31-19-208.ap-northeast-1.compute.internal>
2023-09-13 08:21:28 +0000

    [add]comment_form

--- app/controllers/public/review_comments_controller.rb
+++ app/controllers/public/review_comments_controller.rb
@@ -1,2 +1,42 @@
 class Public::ReviewCommentsController < ApplicationController
+  
+  
+  def create
+    @review = Review.find(params[:review_id])
+    comment = current_user.review_comments.new(review_comment_params)
+    comment.review_id = @review.id
+    @review_comment = ReviewComment.new
+    if comment.save
+      flash[:success] = "コメントが投稿されました。"
+    else
+      flash[:error] = "コメントの投稿に失敗しました。"
+    end
+    redirect_to review_path(@review)
+  end
+
+  def destroy
+    @review = Review.find(params[:review_id])
+    comment = @review.review_comments.find(params[:id])
+    @review_comment = ReviewComment.new
+    if comment.destroy
+      flash[:success] = "コメントが削除されました。"
+    else
+      flash[:error] = "コメントの削除に失敗しました。"
+    end
+    redirect_to review_path(@review)
+  end
+    
+
+  private
+
+  def review_comment_params
+    params.require(:review_comment).permit(:text)
+  end
+
+
 end
+
+
+
+
+
