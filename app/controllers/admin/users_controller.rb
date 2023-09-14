0ab777964e3e121a1ef4c9d97228001c3f1693f7
EC2 Default User <ec2-user@ip-172-31-19-208.ap-northeast-1.compute.internal>
2023-09-14 12:55:05 +0000

    [add]layout

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,7 +6,7 @@ class Admin::UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-  
+
   def destroy
     @user = User.find(params[:id])
     @user.destroy
