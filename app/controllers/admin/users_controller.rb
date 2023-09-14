b8f5f1c5b210963f1e92aff142e3b9cfd59116c3
EC2 Default User <ec2-user@ip-172-31-19-208.ap-northeast-1.compute.internal>
2023-09-14 06:24:15 +0000

    [add]admin-user-show

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,5 +4,20 @@ class Admin::UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
+  
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:user_destroy] = "会員情報を削除しました。"
+    redirect_to admin_users_path
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :text, :image)
+  end
+
 end
