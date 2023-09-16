144b3cd774e2f9f0c5c18b7bc0e674eb9a99a5cf
EC2 Default User <ec2-user@ip-172-31-19-208.ap-northeast-1.compute.internal>
2023-09-16 09:22:32 +0000

    [add]guestlogin

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,26 @@ class User < ApplicationRecord
 
   has_one_attached :profile_image
 
+  GUEST_USER_EMAIL = "guest@example.com"
+
   def get_profile_image
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     profile_image.variant(resize_to_limit: [150, 150]).processed
+  end
+
+
+  def self.guest
+    find_or_create_by!(email: GUEST_USER_EMAIL) do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "guestuser"
+    end
+  end
+
+  def guest_user?
+    email == GUEST_USER_EMAIL
   end
 
 end
