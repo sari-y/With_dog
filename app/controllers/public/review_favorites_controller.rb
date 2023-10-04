8cd0682d5cfe3defabee279e82a45fa194a17235
EC2 Default User <ec2-user@ip-172-31-19-208.ap-northeast-1.compute.internal>
2023-10-04 14:38:00 +0000

    [add]favorite

--- app/controllers/public/review_favorites_controller.rb
+++ app/controllers/public/review_favorites_controller.rb
@@ -6,14 +6,12 @@ class Public::ReviewFavoritesController < ApplicationController
     review = Review.find(params[:review_id])
     favorite = current_user.review_favorites.new(review_id: review.id)
     favorite.save
-    redirect_to request.referer
   end
 
   def destroy
     review = Review.find(params[:review_id])
     favorite = current_user.review_favorites.find_by(review_id: review.id)
     favorite.destroy
-    redirect_to request.referer
   end
 
 end
