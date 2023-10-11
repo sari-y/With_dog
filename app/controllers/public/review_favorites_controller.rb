68b9940f70b80456ca8164c62d87237f29a616c4
EC2 Default User <ec2-user@ip-172-31-19-208.ap-northeast-1.compute.internal>
2023-10-11 11:49:01 +0000

    [fix]favorite

--- app/controllers/public/review_favorites_controller.rb
+++ app/controllers/public/review_favorites_controller.rb
@@ -3,14 +3,14 @@ class Public::ReviewFavoritesController < ApplicationController
 
 
   def create
-    review = Review.find(params[:review_id])
-    favorite = current_user.review_favorites.new(review_id: review.id)
+    @review = Review.find(params[:review_id])
+    favorite = current_user.review_favorites.new(review_id: @review.id)
     favorite.save
   end
 
   def destroy
-    review = Review.find(params[:review_id])
-    favorite = current_user.review_favorites.find_by(review_id: review.id)
+    @review = Review.find(params[:review_id])
+    favorite = current_user.review_favorites.find_by(review_id: @review.id)
     favorite.destroy
   end
 
