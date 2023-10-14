271e9c3e08a7e14e8ad9ba86e21169e84bbb6cb5
EC2 Default User <ec2-user@ip-172-31-19-208.ap-northeast-1.compute.internal>
2023-10-14 09:58:33 +0000

    [add]bookmark

--- app/controllers/public/bookmarks_controller.rb
+++ app/controllers/public/bookmarks_controller.rb
@@ -2,17 +2,13 @@ class Public::BookmarksController < ApplicationController
    before_action :authenticate_user!
 
   def create
-    review = Review.find(params[:review_id])
-    current_user.bookmark(review)
-    flash[:success] = t('bookmark.create')
-    redirect_back(fallback_location: root_path)
+    @review = Review.find(params[:review_id])
+    current_user.bookmark(@review)
   end
 
   def destroy
-    review = current_user.bookmarks.find_by(id: params[:id])&.review
-    current_user.unbookmark(review)
-    flash[:success] = t('bookmark.destroy')
-    redirect_back(fallback_location: root_path)
+    @review = current_user.bookmarks.find_by(id: params[:id])&.review
+    current_user.unbookmark(@review)
   end
 
   def index
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,5 +1,6 @@
 class Bookmark < ApplicationRecord
   belongs_to :user
   belongs_to :review
-  validates_uniqueness_of :user_id, scope: :review_id
+
+  validates :user_id, uniqueness: { scope: :review_id }
 end
